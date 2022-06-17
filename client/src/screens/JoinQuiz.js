f174960e992c04edea10784a3ccc055fb10dc6c8
Vivek <vivek0094.cse19@chitkara.edu.in>
2022-06-18 00:06:36 +0530

    init commit

---
+++ client/src/screens/JoinQuiz.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import './JoinQuiz.css';
+
+const JoinQuiz = () => {
+    const [valid, setValid] = useState('false');
+    const [code, setCode] = useState('');
+
+    const handleJoinQuiz = () => {
+        if (code.length) setValid('attempt-quiz');
+    };
+
+    if (valid !== 'false') return <Redirect push to={`/${valid}/${code}`} />;
+    // if (valid === 'join-blind')
+    // 	return <Redirect push to={`attempt-blind-quiz/${code}`} />
+
+    return (
+        <div id="join-quiz">
+            <div id="join-quiz-div">
+                <div className="logo-name">
+                    <b>Quiz</b>iify
+                </div>
+                <input
+                    value={code}
+                    onChange={(e) => setCode(e.target.value)}
+                    id="q-code"
+                    type="text"
+                    placeholder="Enter Quiz Code"
+                    autoFocus
+                    onKeyPress={(event) => {
+                        if (event.key === 'Enter') handleJoinQuiz();
+                    }}
+                />
+                <button className="join-button" onClick={handleJoinQuiz}>
+                    Join Quiz
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default JoinQuiz;
